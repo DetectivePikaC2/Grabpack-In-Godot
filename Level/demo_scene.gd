df74fbb854b0c40c3617ba2948c0534052d5b7ff
DetectivePikaC2 <166664745+DetectivePikaC2@users.noreply.github.com>
2024-09-07 09:25:50 -0700

    THIS VERSION IS BROKEN DONT USE IT

--- Level/demo_scene.gd
+++ Level/demo_scene.gd
@@ -3,6 +3,7 @@ extends Node3D
 @onready var gate = $puzzle/Gate
 @onready var gate_2 = $puzzle/Gate2
 @onready var player = $Player
+@onready var large_gate_2 = $"puzzle/Large Gate2"
 
 var gate_openned = false
 
@@ -25,3 +26,10 @@ func _on_hand_notify_area_entered(area):
 func _on_grabpack_item_collected():
 	Game.set_objective("Explore", 5)
 	Game.dialog("Use keys 1-4 or MOUSE SCROLL to switch hands", 5)
+
+
+func _on_large_button_pressed():
+	large_gate_2._open()
+
+func _on_large_button_released():
+	large_gate_2._close()
